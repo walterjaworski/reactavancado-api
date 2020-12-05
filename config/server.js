5b7ca24f842e5a7ad9c203a41997bb495fba7be9
waltinhoj <walter.jaworski@gmail.com>
2020-12-05 12:06:42 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36c3321aa2e7e30e267d9c898debb900'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51796373-26dc-435a-9e7d-ec16d1145e31'
+};

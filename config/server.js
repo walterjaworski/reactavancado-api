7a7d995965a26709cb30a9defa7884eb7ab62113
waltinhoj <walter.jaworski@gmail.com>
2020-12-17 19:51:00 -0300

    update jwt token

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
+      // secret: env('ADMIN_JWT_SECRET', '36c3321aa2e7e30e267d9c898debb900'),
       secret: env('ADMIN_JWT_SECRET', 'dl++o3SHPz6/S8aeur0jqgPU4YwITmrgfeD7LtYZ4BUk3U1kUO6cjtofKnbBKNyVH2e00P+3e3ylOa/51lGXGg=='),
     },
   },
